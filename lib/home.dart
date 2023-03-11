3f2455d58a86445b77fd317926c1d806792a5b4d
Harsh Koli <harshkoli201@gmail.com>
2023-03-12 02:27:53 +0530

    Dots on Appbar added

--- lib/home.dart
+++ lib/home.dart
@@ -19,18 +19,39 @@ class Hello1 extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        leading: Container(
-          margin: const EdgeInsets.all(10),
-          alignment: Alignment.center,
-          child: SvgPicture.asset(
-            'assest/icons/Arrow - Left 2.svg',
-            height: 20,
-            width: 20,
-          ),
-          decoration: const BoxDecoration(
-            color: Color(0xffF7F8F8),
+        leading: GestureDetector(
+          onTap: (() {}),
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assest/icons/Arrow - Left 2.svg',
+              height: 20,
+              width: 20,
+            ),
+            decoration: const BoxDecoration(
+              color: Color(0xffF7F8F8),
+            ),
           ),
         ),
+        actions: [
+          GestureDetector(
+            onTap: (() {}),
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 37,
+              child: SvgPicture.asset(
+                'assest/icons/dots.svg',
+                height: 5,
+                width: 5,
+              ),
+              decoration: const BoxDecoration(
+                color: Color(0xffF7F8F8),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
