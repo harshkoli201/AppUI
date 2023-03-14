38cfe2f86d62503bd4eac272ba1fe9d3b58e990e
Harsh Koli <harshkoli201@gmail.com>
2023-03-14 13:53:03 +0530

    SearchField

--- lib/home.dart
+++ lib/home.dart
@@ -7,52 +7,127 @@ class Hello1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+      backgroundColor: Colors.white,
+      appBar: appBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _SearchField(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                height: 150,
+                color: Colors.green,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _SearchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: SvgPicture.asset(
+              'assest/icons/Search.svg',
+            ),
+          ),
+          suffixIcon: Container(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  VerticalDivider(
+                    color: Colors.black,
+                    thickness: 0.1,
+                    indent: 10,
+                    endIndent: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: SvgPicture.asset('assest/icons/Filter.svg'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
           ),
         ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        leading: GestureDetector(
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: (() {}),
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assest/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.transparent,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
           onTap: (() {}),
           child: Container(
             margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 37,
             child: SvgPicture.asset(
-              'assest/icons/Arrow - Left 2.svg',
-              height: 20,
-              width: 20,
+              'assest/icons/dots.svg',
+              height: 5,
+              width: 5,
             ),
             decoration: const BoxDecoration(
-              color: Color(0xffF7F8F8),
+              color: Colors.transparent,
             ),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: (() {}),
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 37,
-              child: SvgPicture.asset(
-                'assest/icons/dots.svg',
-                height: 5,
-                width: 5,
-              ),
-              decoration: const BoxDecoration(
-                color: Color(0xffF7F8F8),
-              ),
-            ),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
